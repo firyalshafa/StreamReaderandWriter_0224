17743666acb41efd959f12e7d7ba76f15e5b238e
firyalshafa <shafafiryal782@gmail.com>
2024-06-04 15:36:53 +0700

    memperbaiki codingan iofilewithpath

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -10,6 +10,10 @@ int main()
     string baris;
     string NamaFile;
 
+    cout << "masukan nama file :";
+    cin >> NamaFile;
+        
+
     // membuka file dalam mode menulis.
     ofstream outfile;
     // menunjuk ke sebuah nama file
